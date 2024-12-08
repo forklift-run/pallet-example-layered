5e7c68179655d1b7c1a3635a0c610a9cf50e24cc
Ethan Li <lietk12@gmail.com>
2024-12-07 20:44:32 -0800

    Fix incorrect `tail` invocation

--- .github/forklift-resolve.sh
+++
@@ -1,5 +0,0 @@
-path="$1"          # path of a pallet or repo, e.g. `github.com/PlanktoScope/pallet-standard`
-version_query="$2" # version query, e.g. `edge` or `main`
-
-version="$(forklift inspector resolve-git-repo "$path@$version_query" | tail)"
-echo "$version"
