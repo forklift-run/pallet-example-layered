4db2246bd605751bb0f38f4edaf7a7bebd8a989a
Ethan Li <lietk12@gmail.com>
2024-12-07 21:53:38 -0800

    Continue troubleshooting

--- .github/forklift-resolve.sh
+++ .github/forklift-resolve.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env -S bash -uo pipefail
+#!/usr/bin/env -S bash -u
 
 path="$1"          # path of a pallet or repo, e.g. `github.com/PlanktoScope/pallet-standard`
 version_query="$2" # version query, e.g. `edge` or `main`; pseudoversions are not supported (yet)
 
-echo "test" >2
-exit 1
-
-version="$(forklift inspector resolve-git-repo "$path@$version_query" | tee /dev/stderr | tail -n 1)"
+output="$(forklift inspector resolve-git-repo "$path@$version_query" 2>&1)"
+retval=$?
+echo "$output" >&2
+if [ "$retval" != 0 ]; then
+  exit "$retval"
+fi
+version="$(echo "$output" | tail -n 1)"
 echo "$version"
